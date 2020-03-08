21c6a9209929e5778d14706a066b12adaeca1e30
Leanxp <leanxp1995@hotmail.com>
2020-03-08 17:03:36 +0000

    Hecho

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const cool = require("cool-ascii-faces");
 const express = require("express");
 
 var app = express();
+var port = process.env.PORT || 80; //Se pone la variable y si no, pues toma el valor 80, el puerto 80 vaya.
 
 app.get("/cool", (request,response) =>{ //Cuando te pida /cool, ejecútame..
 	response.send("<html>"+cool()+"</html>");
 }); 
-app.listen(80);
+app.listen(80, () => { //No es recomendable poner el puerto 80, en heroku habrá que especificarle el puerto.
+	console.log("Server ready");
+});
 
 //El app.use() lo que va a decir es.... "Todo lo que te pida de recurso, lo va a buscar en esa carpeta y te lo va a devolver"
 //Nosotros aqui le hemos dicho... Cuando te manden a /public busca dentro de esa carpeta y si hay una carpeta se lo devuelves
 app.use("/", express.static("./public")); 
 
-console.log("Server ready");
+console.log("Starting server . . . ");
