5001d81eadef0fabe6c978c34807bf3cf36a71c2
Leanxp <leanxp1995@hotmail.com>
2020-03-08 16:14:04 +0000

    New

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const cool = require("cool-ascii-faces");
-console.log(cool());
+const express = require("express");
+
+var app = express();
+
+app.get("/cool", (request,response) =>{ //Cuando te pida /cool, ejecútame..
+	response.send("<html>"+cool()+"</html>");
+}); 
+app.listen(80);
+
+//El app.use() lo que va a decir es.... "Todo lo que te pida de recurso, lo va a buscar en esa carpeta y te lo va a devolver"
+//Nosotros aqui le hemos dicho... Cuando te manden a /public busca dentro de esa carpeta y si hay una carpeta se lo devuelves
+app.use("/", express.static("./public")); 
+
+console.log("Server ready");
