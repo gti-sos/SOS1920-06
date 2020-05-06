1956da5e72fd7f514597ab601d231293da5acd66
Leanxp <leanxp1995@hotmail.com>
2020-05-06 19:10:05 +0200

    hey

--- src/back/acc-stats/index.js
+++ src/back/acc-stats/index.js
@@ -1,11 +1,13 @@
 //..Modularizando
 var api = require("./v1");
+var api2 = require("./v2");
 module.exports = function (app, BASE_PATH) {
 	api(app,BASE_PATH+"/v1");
+	api2(app,BASE_PATH+"/v2");
 }
 
 //..Modularizando
-var api = require("./v2");
-module.exports = function (app, BASE_PATH) {
-	api(app,BASE_PATH+"/v2");
-}
+
+//module.exports = function (app, BASE_PATH) {
+	//api(app,BASE_PATH+"/v2");
+//}
