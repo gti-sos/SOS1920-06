3123c683fd50f21d2a33bb06f21a17a6469bf0dd
adriperezguti <us55AS4M>
2020-06-01 17:36:56 +0200

    video youtube adri

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -4,7 +4,7 @@
 
 <main>
     <ul style="list-style-type:circle">VÍDEOS EXPLICATIVOS
-		<li><a href="https://www.youtube.com/watch?v=iLBBRuVDOo4">Video de Adri</a></li>
+		<li><a href="https://www.youtube.com/watch?v=_Sc2bTgHLDw">Video de Adri</a></li>
 		<li><a href="https://www.youtube.com/watch?v=iLBBRuVDOo4">Vídeo de Álvaro</a></li>
         <li><a href="https://www.youtube.com/watch?v=iLBBRuVDOo4">Vídeo de Leandro</a></li>
     </ul>
