5e4d7451a4ef2b8a74a13cb74aec18a4ee73646a
alvarocp16 <alvarocp16@gmail.com>
2020-06-04 03:06:46 +0200

    D03

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -5,7 +5,7 @@
 <main>
     <ul style="list-style-type:circle">VÍDEOS EXPLICATIVOS
 		<li><a href="https://www.youtube.com/watch?v=_Sc2bTgHLDw">Video de Adri</a></li>
-		<li><a href="https://www.youtube.com/watch?v=iLBBRuVDOo4">Vídeo de Álvaro</a></li>
+		<li><a href="https://www.youtube.com/watch?v=F9jK16Cd1qc">Vídeo de Álvaro</a></li>
         <li><a href="https://www.youtube.com/watch?v=NpknbYUgIWc">Vídeo de Leandro</a></li>
     </ul>
   
